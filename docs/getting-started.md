3bbecf2f9d5494ad65a6131f411b1f75e8cbba1f
Raul <elecash@gmail.com>
2016-08-19 11:03:40 +0200

    fix(fonts): Fix embedded font
    Remove embedded font and add a separated CSS file
    
    close #181

--- docs/getting-started.md
+++ docs/getting-started.md
@@ -5,3 +5,72 @@ currentMenu: getting-started
 ### Getting Started
 
 Getting started
+
+<h3>Install</h3>
+
+Use npm to install Videogular as a dependency:
+
+```bash
+npm install videogular2 --save
+```
+
+Create an Angular application with TypeScript:
+
+```typescript
+import {Component} from '@angular/core';
+import {VgPlayer} from 'videogular2/core';
+import {VgControls, 
+        VgPlayPause, 
+        VgScrubBar, 
+        VgScrubBarCurrentTime, 
+        VgScrubBarBufferingTime, 
+        VgMute, 
+        VgFullscreen} from 'videogular2/controls';
+import {VgOverlayPlay} from 'videogular2/overlay-play';
+
+@Component({
+    selector: 'my-player',
+    templateUrl: 'src/my-player.html',
+    directives: [
+        VgPlayer,
+        VgOverlayPlay,
+        VgControls, 
+        VgPlayPause, 
+        VgScrubBar, 
+        VgScrubBarCurrentTime, 
+        VgScrubBarBufferingTime, 
+        VgMute, 
+        VgFullscreen
+    ]
+})
+export class MyPlayer {}
+
+bootstrap(MyPlayer);
+```
+
+Create your video player with HTML in your template:
+
+```html
+<vg-player>
+    <vg-overlay-play></vg-overlay-play>
+
+    <vg-controls>
+        <vg-play-pause></vg-play-pause>
+        <vg-time-display>{{ media?.time?.current | date:'mm:ss' }}</vg-time-display>
+        <vg-scrub-bar>
+            <vg-scrub-bar-current-time></vg-scrub-bar-current-time>
+            <vg-scrub-bar-buffering-time></vg-scrub-bar-buffering-time>
+        </vg-scrub-bar>
+        <vg-time-display>{{ media?.time?.left | date:'mm:ss' }}</vg-time-display>
+        <vg-mute></vg-mute>
+        <vg-fullscreen></vg-fullscreen>
+    </vg-controls>
+
+    <video #media id="singleVideo" preload="auto">
+        <source src="http://static.videogular.com/assets/videos/videogular.mp4" type="video/mp4">
+        <source src="http://static.videogular.com/assets/videos/videogular.ogg" type="video/ogg">
+        <source src="http://static.videogular.com/assets/videos/videogular.webm" type="video/webm">
+    </video>
+</vg-player>
+```
+
--- src/vg-player/vg-player.ts
+++ src/vg-player/vg-player.ts
@@ -20,17 +20,6 @@ import {VgMedia} from "../vg-media/vg-media";
     directives: [VgMedia],
     template: `<ng-content></ng-content>`,
     styles: [`
-        @font-face {
-            font-family: 'videogular';
-            src: url('node_modules/videogular2/fonts/videogular.eot');
-            src: url('node_modules/videogular2/fonts/videogular.eot?#iefix') format('embedded-opentype'),
-                 url('node_modules/videogular2/fonts/videogular.woff') format('woff'),
-                 url('node_modules/videogular2/fonts/videogular.ttf') format('truetype'),
-                 url('node_modules/videogular2/fonts/videogular.svg#videogular') format('svg');
-            font-weight: normal;
-            font-style: normal;
-        }
-
         :host {
             font-family: 'videogular';
             position: relative;
@@ -54,22 +43,22 @@ import {VgMedia} from "../vg-media/vg-media";
     `]
 })
 export class VgPlayer implements AfterContentInit {
-    elem:HTMLElement;
-    api:VgAPI;
+    elem: HTMLElement;
+    api: VgAPI;
 
-    @HostBinding('class.fullscreen') isFullscreen:boolean = false;
-    @HostBinding('style.z-index') zIndex:string;
-
-    @Output()
-    onPlayerReady:EventEmitter<any> = new EventEmitter();
+    @HostBinding('class.fullscreen') isFullscreen: boolean = false;
+    @HostBinding('style.z-index') zIndex: string;
 
     @Output()
-    onMediaReady:EventEmitter<any> = new EventEmitter();
+    onPlayerReady: EventEmitter<any> = new EventEmitter();
+
+    @Output()
+    onMediaReady: EventEmitter<any> = new EventEmitter();
 
     @ContentChildren(VgMedia)
-    medias:QueryList<VgMedia>;
+    medias: QueryList<VgMedia>;
 
-    constructor(ref:ElementRef, api:VgAPI) {
+    constructor(ref: ElementRef, api: VgAPI) {
         this.api = api;
         this.elem = ref.nativeElement;
 
@@ -80,7 +69,7 @@ export class VgPlayer implements AfterContentInit {
         this.medias.toArray().forEach((media) => {
             this.api.registerMedia(media);
         });
-        
+
         this.api.onPlayerReady();
         this.onPlayerReady.next(this.api);
 
