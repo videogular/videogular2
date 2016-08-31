40e127a32aed3055133ccace88404824ff4aab8a
Joan Llenas <joan.llenas.maso@gmail.com>
2016-08-31 14:42:18 +0200

    docs(vr-player example): Enhanced example and fixed RC5 support

--- examples/vr-player/src/vr-player.ts
+++ examples/vr-player/src/vr-player.ts
@@ -8,21 +8,21 @@ import {VgFullscreenAPI} from "videogular2/core";
 export class VRPlayer implements OnInit {
     elem:any;
     aframe:any;
-    dialog:any;
-    progress:any;
-    color:number = 34034;
-    lastTime:number = 0;
-    hexColor:string = '#0084f2';
-    videoUrl:string = 'https://ucarecdn.com/bcece0a8-86ce-460e-856b-40dac4875f15/';
+    videoUrls:Array<string> = [
+        'https://ucarecdn.com/bcece0a8-86ce-460e-856b-40dac4875f15/',
+        'http://static.videogular.com/assets/videos/vr-demo.mp4'
+    ];
+    videoUrl:string;
+    spinning:boolean;
 
     constructor(ref:ElementRef) {
         this.elem = ref.nativeElement;
+        this.videoUrl = this.videoUrls[0];
+        this.spinning = false;
     }
 
     ngOnInit() {
-        this.dialog = this.elem.querySelector('.info-dialog');
         this.aframe = this.elem.querySelector('a-scene');
-
         VgFullscreenAPI.onChangeFullscreen.subscribe(this.onChangeFullscreen.bind(this));
     }
 
@@ -34,26 +34,13 @@ export class VRPlayer implements OnInit {
     }
 
     onMouseEnter($event) {
-        this.progress = requestAnimationFrame(currentTime => this.onProgress(currentTime, $event));
-    }
-
-    onProgress(currentTime, $event) {
-        if (!this.lastTime) this.lastTime = currentTime;
-
-        let elapsedTime:number = currentTime - this.lastTime;
-
-
-        if (elapsedTime > 3000) {
-            cancelAnimationFrame(this.progress.data.handleId);
-            this.lastTime = 0;
-        }
-        else {
-            let hexNum = Number(this.color).toString(16);
-
-            this.progress = requestAnimationFrame(currentTime => this.onProgress(currentTime, $event));
-            this.color += 1;
-            this.hexColor = '#' + Array(6 - hexNum.length + 1).join('0') + hexNum;
-            $event.target.setAttribute('color', this.hexColor);
+        if(!this.spinning) {
+            this.spinning = true;
+            document.querySelector('#infoImage')['emit']('startSpinning');
+            setTimeout( () => {
+                this.videoUrl = this.videoUrls.reverse()[0]
+                this.spinning = false;
+            }, 1250 );
         }
     }
 }
