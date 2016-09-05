96cef060936801ca951f199965aa0b02be749076
Joan Llenas <joan.llenas.maso@gmail.com>
2016-09-05 16:39:16 +0200

    feat(Angular version): Updated to Angular2 RC6

--- karma.conf.js
+++ karma.conf.js
@@ -25,20 +25,24 @@ module.exports = function(config) {
 
             // Zone.js dependencies
             'node_modules/zone.js/dist/zone.js',
+            'node_modules/zone.js/dist/long-stack-trace-zone.js',
+            'node_modules/zone.js/dist/proxy.js',
+            'node_modules/zone.js/dist/sync-test.js',
             'node_modules/zone.js/dist/jasmine-patch.js',
             'node_modules/zone.js/dist/async-test.js',
+            'node_modules/zone.js/dist/fake-async-test.js',
 
             // RxJs.
             { pattern: 'node_modules/rxjs/**/*.js', included: false, watched: false },
             { pattern: 'node_modules/rxjs/**/*.js.map', included: false, watched: false },
 
 
-            {pattern: 'karma-test-shim.js', included: true, watched: true},
-
             // paths loaded via module imports
             // Angular itself
             {pattern: 'node_modules/@angular/**/*.js', included: false, watched: true},
             {pattern: 'node_modules/@angular/**/*.js.map', included: false, watched: true},
+
+            {pattern: 'karma-test-shim.js', included: true, watched: true},
 
             // Our built application code
             {pattern: 'src/**/*.js', included: false, watched: true},
@@ -49,8 +53,7 @@ module.exports = function(config) {
             // {pattern: 'dist/**/*.css', included: false, watched: true},
 
             // paths to support debugging with source maps in dev tools
-            {pattern: 'src/**/*.ts', included: false, watched: false},
-            {pattern: 'src/**/*.js.map', included: false, watched: false}
+            {pattern: 'src/**/*.ts', included: false, watched: false}
         ],
 
         // proxied base paths
