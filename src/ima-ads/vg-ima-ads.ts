5d7501b4cbe9b277bdd21f5878dc2bd10b723ec1
Alouane Nour-Eddine <alouane00@gmail.com>
2017-04-09 13:38:00 +0100

    workaround for mobile linear ads
     =>  freezing issue

--- src/ima-ads/vg-ima-ads.ts
+++ src/ima-ads/vg-ima-ads.ts
@@ -48,7 +48,16 @@ export class VgImaAds {
         this.API.playerReadyEvent.subscribe((api) => this.onPlayerReady());
     }
 
+    playhandler = function(th):void {
+         if(!this.alreadyfixed) document.getElementsByTagName('video')[0].play();
+        this.alreadyfixed = true;   
+    }
+    
     ngOnInit() {
+         //Trigger the handler function after a click event on the video container => mandatory for the mobile ads
+        let adplayer = document.querySelector(".video");
+        adplayer.addEventListener('click', this.playhandler);
+        
         if (this.API.isPlayerReady) {
             this.onPlayerReady();
         }
