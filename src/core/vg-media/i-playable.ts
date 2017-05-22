009e8d0dcf201907a9bce3d480608e2a765770e4
jorgeucano <jorgeucano@gmail.com>
2017-05-22 11:39:16 -0300

    Add create track and cues dynamically

--- src/core/vg-media/i-playable.ts
+++ src/core/vg-media/i-playable.ts
@@ -5,7 +5,7 @@ export interface IPlayable {
     elem:any;
     time:any;
     buffer:any;
-    track?:any; // its optional, need in add custom cue (addTextTrack)     
+    track?:any;
     canPlay:boolean;
     canPlayThrough:boolean;
     isMetadataLoaded:boolean;
