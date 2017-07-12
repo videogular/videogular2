e7c8f10716015f62243dd0489e3fab374276e4d1
kdlarson <kdlarson@users.noreply.github.com>
2017-07-11 17:04:53 -0700

    Added Azuer Media Services manifest detection

--- src/streaming/vg-hls/vg-hls.ts
+++ src/streaming/vg-hls/vg-hls.ts
@@ -78,7 +78,7 @@ export class VgHLS implements OnInit, OnChanges, OnDestroy {
         }
 
         // It's a HLS source
-        if (this.vgHls && this.vgHls.indexOf('.m3u8') > -1 && Hls.isSupported()) {
+        if (this.vgHls && ((this.vgHls.indexOf('.m3u8') > 1) || (this.vgHls.indexOf('m3u8-aapl') > -1)) && Hls.isSupported()) {
             let video:HTMLVideoElement = this.ref.nativeElement;
 
             this.hls = new Hls(this.config);
