6f4f7b1d4cd5ced6735c6a08f10a109d8918ca35
Raul <elecash@gmail.com>
2018-04-01 12:11:08 +0200

    fix(controls): Fix track options visibility
    Fix for windows only, now the tracks names are invisible anymore
    
    #691

--- src/controls/vg-track-selector/vg-track-selector.ts
+++ src/controls/vg-track-selector/vg-track-selector.ts
@@ -67,6 +67,9 @@ export interface Option {
             color: transparent;
             font-size: 16px;
         }
+        vg-track-selector select.trackSelector option {
+            color: #000;
+        }
         vg-track-selector .track-selected {
             position: absolute;
             width: 100%;
