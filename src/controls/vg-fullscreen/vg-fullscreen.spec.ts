5ecc68f0011a57d05610a420556dc2f841ec42fb
Eric Solari <sliceofbytes@gmail.com>
2019-06-06 12:06:59 -0500

    test(Karma): Update spec files to pass typescript type checking.

--- src/controls/vg-fullscreen/vg-fullscreen.spec.ts
+++ src/controls/vg-fullscreen/vg-fullscreen.spec.ts
@@ -1,56 +1,56 @@
-import {VgFullscreen} from "./vg-fullscreen";
-import {VgAPI} from "../../core/services/vg-api";
-import {ElementRef} from "@angular/core";
-import {VgFullscreenAPI} from "../../core/services/vg-fullscreen-api";
+import { VgFullscreen } from './vg-fullscreen';
+import { VgAPI } from '../../core/services/vg-api';
+import { ElementRef } from '@angular/core';
+import { VgFullscreenAPI } from '../../core/services/vg-fullscreen-api';
 
 describe('Videogular Player', () => {
-    let fullscreen: VgFullscreen;
-    let ref:ElementRef;
-    let api:VgAPI;
-    let fsAPI:VgFullscreenAPI;
+	let fullscreen: VgFullscreen;
+	let ref: ElementRef;
+	let api: VgAPI;
+	let fsAPI: VgFullscreenAPI;
 
-    beforeEach(() => {
-        ref = {
-            nativeElement: {
-                getAttribute: (name) => {
-                    return name;
-                }
-            }
-        };
+	beforeEach(() => {
+		ref = {
+			nativeElement: {
+				getAttribute: (name) => {
+					return name;
+				}
+			}
+		};
 
-        api = new VgAPI();
-        fsAPI = new VgFullscreenAPI();
-        fullscreen = new VgFullscreen(ref, api, fsAPI);
-    });
+		api = new VgAPI();
+		fsAPI = new VgFullscreenAPI();
+		fullscreen = new VgFullscreen(ref, api, fsAPI);
+	});
 
-    it('Should get media by id on init', () => {
-        spyOn(api, 'getMediaById').and.callFake(() => { });
+	it('Should get media by id on init', () => {
+		spyOn(api, 'getMediaById').and.callFake(() => <any>{});
 
-        fullscreen.vgFor = 'test';
-        fullscreen.onPlayerReady();
+		fullscreen.vgFor = 'test';
+		fullscreen.onPlayerReady();
 
-        expect(api.getMediaById).toHaveBeenCalledWith('test');
-    });
+		expect(api.getMediaById).toHaveBeenCalledWith('test');
+	});
 
-    describe('onClick', () => {
-        beforeEach(() => {
-            spyOn(fsAPI, 'toggleFullscreen');
-        });
+	describe('onClick', () => {
+		beforeEach(() => {
+			spyOn(fsAPI, 'toggleFullscreen');
+		});
 
-        it('Should call toggleFullscreen with null param if target is API', () => {
-            fullscreen.target = api;
+		it('Should call toggleFullscreen with null param if target is API', () => {
+			fullscreen.target = api;
 
-            fullscreen.onClick();
+			fullscreen.onClick();
 
-            expect(fsAPI.toggleFullscreen).toHaveBeenCalledWith(null);
-        });
+			expect(fsAPI.toggleFullscreen).toHaveBeenCalledWith(null);
+		});
 
-        it('Should call toggleFullscreen with target param if target', () => {
-            fullscreen.target = 'test';
+		it('Should call toggleFullscreen with target param if target', () => {
+			fullscreen.target = 'test';
 
-            fullscreen.onClick();
+			fullscreen.onClick();
 
-            expect(fsAPI.toggleFullscreen).toHaveBeenCalledWith('test');
-        });
-    });
+			expect(fsAPI.toggleFullscreen).toHaveBeenCalledWith('test');
+		});
+	});
 });
--- src/core/services/vg-utils.spec.ts
+++ src/core/services/vg-utils.spec.ts
@@ -1,34 +1,30 @@
-import {VgUtils} from "./vg-utils";
+import { VgUtils } from './vg-utils';
 
 describe('Videogular Utils', () => {
-    it('Should get the highest z-index', () => {
-        spyOn(window, 'getComputedStyle').and.callFake(() => {
-            return {
-                'z-index': 2
-            };
-        });
+	it('Should get the highest z-index', () => {
+		spyOn(window, 'getComputedStyle').and.callFake(() => <any>{ 'z-index': 2 });
 
-        let highestZ = VgUtils.getZIndex();
+		let highestZ = VgUtils.getZIndex();
 
-        expect(highestZ).toBe(3);
-    });
+		expect(highestZ).toBe(3);
+	});
 
-    it('Should get if is a mobile device', () => {
-        // window.orientation is not writable
-        let isMobileDevice = VgUtils.isMobileDevice();
+	it('Should get if is a mobile device', () => {
+		// window.orientation is not writable
+		let isMobileDevice = VgUtils.isMobileDevice();
 
-        expect(isMobileDevice).toBeFalsy();
-    });
+		expect(isMobileDevice).toBeFalsy();
+	});
 
-    it('Should get if is an iOS device', () => {
-        let isiOS = VgUtils.isiOSDevice();
+	it('Should get if is an iOS device', () => {
+		let isiOS = VgUtils.isiOSDevice();
 
-        expect(isiOS).toBeFalsy();
-    });
+		expect(isiOS).toBeFalsy();
+	});
 
-    it('Should get if is a Cordova app', () => {
-        let isCordova = VgUtils.isCordova();
+	it('Should get if is a Cordova app', () => {
+		let isCordova = VgUtils.isCordova();
 
-        expect(isCordova).toBeFalsy();
-    });
+		expect(isCordova).toBeFalsy();
+	});
 });
